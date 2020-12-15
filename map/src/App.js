55178ba98a65c9cc8ce83b34557e3d2d23d13a59
Yang Zhou <yzhou3991@gmail.com>
2020-12-15 10:36:56 -0500

    make map full screen

--- map/src/App.js
+++ map/src/App.js
@@ -1,19 +1,25 @@
 import React,{useState} from 'react'
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, {Marker} from 'react-map-gl'
 
 export default function App(){
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
+    width: "100vw",
+    height: "100vh",
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 8
   });
 
-  return (
-    <ReactMapGL
+  return ( 
+    <div>
+      <ReactMapGL
       {...viewport}
+      mapStyle = 'mapbox://styles/yangzhou93/ckipmnmy613zi17ti10exzems'
       onViewportChange={nextViewport => setViewport(nextViewport)}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}/>
+      
+    </div>
+      
+
   );
 }
