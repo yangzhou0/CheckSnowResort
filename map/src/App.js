ecd573b0e31493cf0074d2f0005e699ebc3b2105
Yang Zhou <yzhou3991@gmail.com>
2020-12-15 01:52:05 -0500

    use mapbox api

--- map/src/App.js
+++ map/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import ReactMapGL from 'react-map-gl'
 
-function App() {
+export default function App(){
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}/>
   );
 }
-
-export default App;
