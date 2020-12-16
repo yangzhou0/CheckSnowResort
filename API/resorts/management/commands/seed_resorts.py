7de5c96c185a6cdd7dae4c8bbe06c016e509a5f3
Yang Zhou <yzhou3991@gmail.com>
2020-12-16 14:03:11 -0500

    write custom command and build django routes and models

---
+++ API/resorts/management/commands/seed_resorts.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+# from resorts.models import Resort
+# import ../../data/resorts/resorts.json as resorts_data
+import json
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        # seed_resorts()
+        # clear_data()
+        test()
+        print("completed")
+
+def seed_resorts():
+    resorts_data = json.loads(resorts_data)
+    for resort in resorts_data:
+        resort = Resort(
+            name = resort['name'],
+            description = resort['description'],
+            latitude = resort['coordinates'][0],
+            longitude = resort['coordinates'][1],
+            website = resort['website'],
+            likes = 0
+        )
+        resort.save()
+
+# def clear_data():
+#   Resort.objects.all().delete()
+
+def test():
+    print('test command')
+
--- API/resorts/models.py
+++ API/resorts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+# Create your models here.
+
+class Resort(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=5000)
+    likes = models.PositiveIntegerField(default = 0)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    website = models.CharField(max_length=200)
+        
+class Comment(models.Model):
+    body = models.CharField(max_length=5000)
+    resort = models.ForeignKey(Resort, on_delete=models.CASCADE,related_name='comments')
+    likes = models.PositiveIntegerField(default = 0)
