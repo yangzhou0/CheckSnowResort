10db087a9162d730b9573756402a7f273c14dc51
Yang Zhou <yzhou3991@gmail.com>
2020-12-28 14:49:08 -0500

    change update and delete back to buttons

--- map/src/components/Comment.js
+++ map/src/components/Comment.js
@@ -1,17 +1,20 @@
 import React,{useState, useEffect} from 'react'
 import { Button } from 'reactstrap';
+import './css/commentBoxComponent.css'
 
 export default function Comment({comment,handleLikeComment,handleUpdateComment,handleDeleteComment,handleAddComment}) {
   const [edit, setEdit] = useState(false);
   return (
-    <div>
+    <div className="comment">
       {!edit ? 
-        <div><span onDoubleClick={()=> setEdit(!edit)}>{comment.body}</span> <Button onClick = {()=>{handleLikeComment(comment.id)}} >like {comment.likes}</Button></div>
+        <div className="comment-body"><span onDoubleClick={()=> setEdit(!edit)}>{comment.body}</span> <Button onClick = {()=>{handleLikeComment(comment.id)}} >like {comment.likes}</Button></div>
       :
-        <div>
+        <div className="comment-body">
           <textarea id = {`textarea${comment.id}`}onDoubleClick={()=> setEdit(!edit)}>{comment.body}</textarea> 
-          <Button onClick = {()=>{handleUpdateComment({id:comment.id,body:document.getElementById(`textarea${comment.id}`).value}); setEdit(!edit)}}>Update</Button>
-          <Button onClick = {()=>{handleDeleteComment(comment.id);setEdit(!edit)}}>Delete</Button>
+          <div className="comment-footer">
+            <Button className="comment-footer-delete" onClick = {()=>{handleUpdateComment({id:comment.id,body:document.getElementById(`textarea${comment.id}`).value}); setEdit(!edit)}}>Update</Button>
+            <Button className="comment-footer-delete" onClick = {()=>{handleDeleteComment(comment.id);setEdit(!edit)}}>Delete</Button>
+          </div>
         </div>
       }
     </div>
